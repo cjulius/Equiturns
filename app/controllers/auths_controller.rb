070cb54d89db3bf04a5443e7bca983efd3a1f8bd
CJulius <charlesejulius@gmail.com>
2014-02-07 16:30:48 -0800

    Login working

--- app/controllers/auths_controller.rb
+++ app/controllers/auths_controller.rb
@@ -9,7 +9,7 @@ class AuthsController < ApplicationController
 
 	def create
 		user = User.find_by(username: params[:user][:username])
-		if user.authenticates_with_password?(params[:user][:password])
+		if user.check_password(params[:user][:password])
 			session[:user_id] = user.id
 			flash[:notice] = "Welcome, #{current_user.username}!"
 			redirect_to root_path
